0993d7bb1236d3d8dc20b1eea5c49078a85ccfb8
Marvin <gudiellopezmarvin@gmail.com>
2024-06-15 08:59:05 -0600

    Cambios de compilacion

---
+++ p1final2024-master/p1final2024-master/p1final2024/Articulo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace p1final2024
+{
+    public class Articulo  // Cambiado a public
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public byte[] Imagen { get; set; }
+    }
+}
+
---
+++ p1final2024-master/p1final2024-master/p1final2024/Articulos.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p1final2024
+{
+    public partial class Articulos : Form
+    {
+        ArticuloDAO articuloDAO = new ArticuloDAO();
+
+        public Articulos()
+        {
+            InitializeComponent();
+            listarArticulos();
+        }
+
+        private void listarArticulos()
+        {
+            dgvArticulos.DataSource = articuloDAO.ReadAll();
+            dgvArticulos.Columns["Imagen"].Visible = false;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Archivos de imagen (*.jpg;*.jpeg;*.png;*.bmp)|*.jpg;*.jpeg;*.png;*.bmp|Todos los archivos (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Obtiene la ruta completa del archivo seleccionado
+                    string filePath = openFileDialog.FileName;
+
+                    // Carga la imagen en el PictureBox
+                    pcbImagen.Image = Image.FromFile(filePath);
+                }
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            guardarNuevo();
+            listarArticulos();
+        }
+
+        private void guardarNuevo()
+        {
+            Articulo articulo = new Articulo
+            {
+                Nombre = txtNombre.Text,
+                Descripcion = txtDescripcion.Text,
+                Precio = Convert.ToDecimal(txtPrecio.Text),
+                Imagen = obtenerImagen()
+            };
+
+            articuloDAO.Create(articulo);
+        }
+
+        private byte[] obtenerImagen()
+        {
+            if (pcbImagen.Image == null)
+            {
+                MessageBox.Show("Por favor, selecciona una imagen.");
+                return null;
+            }
+
+            using (var ms = new System.IO.MemoryStream())
+            {
+                pcbImagen.Image.Save(ms, pcbImagen.Image.RawFormat);
+                return ms.ToArray();
+            }
+        }
+    }
+}
+
